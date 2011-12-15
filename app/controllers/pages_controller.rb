f34083a0a95e62dd1aaee0688f980b221ed445f3
Yuki Nagato <foxnewsnetwork@gmail.com>
2011-12-15 11:26:02 -0800

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def developer
+  end
+
+  def contact
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freenet::Application.config.secret_token = '6a073d52472235e3e57bef52920e39cef148392a8280d1119bf4d834fd41539d5477e10a8aea554b3a22c34fba68ee7a439bd29e327e8013958dff374f472e82'
